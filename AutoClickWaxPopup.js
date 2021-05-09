2d006bfdc2bc97882c54c01ad5078f432fe630e6
idhuna <parunyu-_-@hotmail.com>
2021-05-09 22:20:27 +0700

    Update AutoClickWaxPopup.js

--- AutoClickWaxPopup.js
+++ AutoClickWaxPopup.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name         Auto Click WaxPopup
+// @namespace    Auto Click WaxPopup
+// @version      1
+// @match        https://all-access.wax.io/cloud-wallet/signing/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
 (async function() {
     console.log('Script Auto Click Popup...')
     let node = document.createElement("span");
@@ -5,16 +14,22 @@
     node.style.marginLeft = "1rem";
     node.textContent = 0;
     let count = 0;
+    let isFound = false;
     setInterval(_ => {
-        if(document.querySelector("div.react-ripples > button")?.disabled == false){
-            document.querySelector("div.react-ripples > button").click();
-        }
-        if(document.querySelector("#root > div > section > div.page-inner-container > div > h1")){
+        if(!isFound && document.querySelector("#root > div > section > div.page-inner-container > div > h1")){
             document.querySelector("#root > div > section > div.page-inner-container > div > h1").append(node);
+            isFound = true;
         }
         node.textContent = count;
         count += 1;
     }, 1000)
+    let intervalTime = 200;
+    setInterval(_ => {
+        if(document.querySelector("div.react-ripples > button")?.disabled == false){
+            document.querySelector("div.react-ripples > button").click();
+            intervalTime = 5000;
+        }
+    },intervalTime)
     setTimeout(_ => {
         document.querySelector("#root > div > section > div.page-inner-container > div > button").click();
     },90*1000);
