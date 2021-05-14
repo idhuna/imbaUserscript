b7b3026bc80034c4bdec6db3a417c84a7622236e
idhuna <parunyu-_-@hotmail.com>
2021-05-15 00:42:40 +0700

    Update AutoStartAlien.js

--- AutoStartAlien.js
+++ AutoStartAlien.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Alien
 // @namespace    Auto Alien
-// @version      1.5
+// @version      1.6
 // @match        https://auto-alien.com/*
 // @updateURL    https://raw.githubusercontent.com/idhuna/imbaUserscript/master/AutoStartAlien.js
 // @downloadURL  https://raw.githubusercontent.com/idhuna/imbaUserscript/master/AutoStartAlien.js
@@ -11,15 +11,19 @@
 (async function() {
     // Window intercept
     console.log('Script Start Auto Alien ...')
-    const winOpen = window.open;
     const delay = ms => new Promise(resolve => setTimeout(_ => resolve(), ms));
-    const tuud = () => {
-        var tuud = new Audio('https://www.soundjay.com/button/button-6.wav');
-        tuud.play();
+    const tuud = (number) => {
+        var context = new AudioContext();
+        let audio = new Audio('https://www.soundjay.com/button/button-'+JSON.parse(number)+'.wav');
+        context.resume().then(() => {
+            audio.play();
+        });
     }
+
+    var winOpen = window.open;
     window.open = function() {
         var win = winOpen.apply(this, arguments);
-        tuud();
+        tuud(7);
         return win;
     };
 
@@ -43,7 +47,6 @@
     // Timeout
     var myTimeout;
     var myTimeout1;
-    var tuud1 = new Audio('https://www.soundjay.com/button/button-4.wav');
     function createReloadTimeout(millis) {
         myTimeout = setTimeout(_ => {
             setTimeout(_ => {location.reload(true)},2000);
@@ -51,7 +54,7 @@
     }
     function createReloadTimeout1(millis) {
         myTimeout1 = setTimeout(_ => {
-            tuud1.play();
+            tuud(4);
             console.log(millis);
             setTimeout(_ => {location.reload(true)},2000);
         },millis);
@@ -70,24 +73,41 @@
 
     // MutationObserver
     const targetNode = document.querySelector("#show-status");
-
+    var oldStatus = undefined;
     const config = { attributes: true, childList: true, subtree: true };
-    const callback = function(mutationsList, observer) {
+    const callback = async function(mutationsList, observer) {
+        let mineDelay
         mutationsList.forEach(function(mutation) {
             if(mutation.type == "childList"){
                 stopReloadTimeout();
                 document.querySelector("#show-status").scrollIntoView(false);
                 scrollBy(0,10);
-                if(document.querySelector("#show-status").textContent == "Check out Recaptcha. "){
+                let str = document.querySelector("#show-status").textContent;
+                if(oldStatus == str) location.reload();
+                if(str == "Check out Recaptcha. "){
                     createReloadTimeout1(3*60000);
+                    return;
                 }
                 createReloadTimeout(30*60000);
                 let re = /\d+/;
-                let str = document.querySelector("#show-status").textContent;
                 let found = str.match(re) || 0;
-                if(parseInt(found[0]) > 0){
-                    let mineDelay = parseInt(found[0])*1000;
+                // Create Delay
+                if(found == 0) return;
+                let nextMineDelay = parseInt(found[0])
+                let found2 = str.match(/Next Mine in/);
+                if(found2){
+                    mineDelay = nextMineDelay;
+                }else{
+                    mineDelay = nextMineDelay * 1000;
+                }
+                if(mineDelay > 0){
+                    oldStatus = str;
                     node.textContent = newTime(new Date().getTime() + mineDelay);
+                    setTimeout(_ => {
+                        // console.log('Start new observer');
+                        observer.observe(targetNode, config);
+                    },mineDelay+2000);
+                    observer.disconnect();
                 }
             }
         });
@@ -95,4 +115,12 @@
 
     const observer = new MutationObserver(callback);
     observer.observe(targetNode, config);
+
+    window.addEventListener("message", (event) => {
+        if(event.origin !== "https://all-access.wax.io") return;
+        if(event.data == 'justReload') {
+            console.log('postMessage',event.data);
+            window.location.reload();
+        }
+    }, false);
 })();
